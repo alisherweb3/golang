0b593850e1f10d3f7953d1888e983fcac54c59c7
alisherweb3 <42702073+alisherweb3@users.noreply.github.com>
2022-10-20 17:27:20 +0500

    Create loops.go

---
+++ loops.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+  for i := 0; i < 100; i++ {
+    if i%20 == 0 {
+      continue
+    }
+    if i == 95 {
+      break
+    }
+    
+    fmt.Print(i, " ")
+  }     
