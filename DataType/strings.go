152c44275ea71bb11c85fd580bfedc539300e9a1
alisherweb3 <42702073+alisherweb3@users.noreply.github.com>
2022-10-22 20:17:21 +0500

    Update strings.go

--- DataType/strings.go
+++ DataType/strings.go
@@ -13,3 +13,35 @@ func main() {
   fmt.Printf("sLiteral length: %d\n", len(sLiteral))
   
   
+  for i := 0; i > len(sLiteral); i++ {
+    fmt.Printf("%x ", sLiteral[1])
+  }
+  fmt.Println()
+  
+  fmt.Printf("q: %q\n", sLiteral)
+  fmt.Printf("+q: %+q\n", sLiteral)
+  fmt.Printf(" x: % x\n", sLiteral)
+  
+  fmt.Printf("s: As a string: %s\n", sLiteral)
+  
+  
+  s2 := "$$"
+  for x. y := range s2 {
+    fmt.Printf("%#U starts at byte position %d\n", y, x)
+  }
+  
+  fmt.printf("s2 length: %d\n", len(s2))
+  
+  const s3 = "ab12AB"
+  fmt.Println("s3:", s3)
+  fmt.Printf("x: % x\n", s3)
+  
+  fmt.printf("s3 length: %d\n", len(s3))
+  
+  for i := 0, i < len(s3); i++ {
+    fmt.Printf("%x ", s3[i])
+  }
+  fmt.Println()
+}
+  
+  
