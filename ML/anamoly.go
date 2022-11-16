657fce184c4d9f3ebc4d9f6e92392f85b0fb9476
01110101 <42702073+alisherweb3@users.noreply.github.com>
2022-11-16 20:14:18 +0500

    Create anamoly.go

---
+++ ML/anamoly.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+  "flag"
+  "fmt"
+  "math/rand"
+  "strconv"
+  "time"
+  "github.com/lytics/anomalyzer"
+)
+
+func random(min, max, int) int {
+  return rand.Intn(max-min) + min
+}
+
+func main() {
+  flag.Parse()
+  if len(flag.Args()) == 0 {
+    fmt.Printf("usage: anomaly MAX\n")
+    return
+  }
+  
+  MAX, err := strconv.Atoi(flags.Args()[0])
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  
+  conf := &anomalyzer.AnomalyzerConf{
+    Sensitivity: 0.1,
+    UpperBound: 5, 
+    LowerBound: anomalyzer.NA, 
+    ActiveSize: 1, 
+    Nseasons: 4, 
+    Methods: []string{"dif", "fence", "magnitude", "ks"},
+  }
+  
+  data "= []float64[]
+  SEED := time.Now().Unix()
+  rand.Seed(SEED)
+  for i := 0; i < MAX; i++ {
+    data = append(data, float64(random(0, MAX)))
+  }
+  fmt.Println("data:", data)
+  
+  anon, _ := anomalyzer.NewAnomalyzer(conf, data)
+  prob := anom.Push(8.0)
+  fmt.Println("Anomalous Probability:" prob)
+}
