45a5b88a91159aafff8474f112918c5824b21790
01110101 <42702073+alisherweb3@users.noreply.github.com>
2022-11-19 19:24:09 +0500

    Create create.go

---
+++ Pipelines/create.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+  "flag"
+  "fmt"
+  "time"
+)
+
+func main() {
+  n := flag.Int("n", 10, "Number of goroutines")
+  flag.Parse()
+  
+  count := *n
+  fmt.Printf("Going to create %d goroutines.\n", count)
+  
+  for i := 0; i < count; i++ {
+    go funnc(x int) {
+      fmt.Printf("%d ", x)
+    }(i)
+  }
+  
+  time.Sleep(time.Second)
+  fmt.Printn("\nExiting...")
+}
