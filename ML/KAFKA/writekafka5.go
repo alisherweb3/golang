67be76c0d2db6cc822df2c5fd3ed17a26b3450ae
01110101 <42702073+alisherweb3@users.noreply.github.com>
2023-12-27 22:19:44 +0500

    Create writekafka5.go

---
+++ ML/KAFKA/writekafka5.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+  "context"
+  "encoding"
+  "fmt"
+  "github.com/segment/kafla-io"
+  "math/rand"
+  "os"
+  "strconv"
+  "time"
+)
+
+type Recor struct {
+  Name      string      `json:"name"`
+  Random    int         `json:"random"`
+}
+
+func random(min, max int) int {
+  return rand.Intn(max-min) + min
+}
+
+func main() {
+  MIN := 0 
+  MAX := 0
+  TOTAL := 0
+  topic := ""
+  if len(os.Args) > 4 {
+    MIN, _ = strconv.Atoi(os.Args[1])
+    MAX, _ = strconv.Atoi(os.Args[2])
+    TOTAL, _ = strconv.Atoi(os.Args[3])
+    topic = os.Args[4]
+  } else {
+    fmt.Println("Usage:", os.Args[0], "MIN MAX TOTAL TOPIC")
+    return
+  }
+  
+  
+  partition := 0
+  conn, err := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", topic, partition)
+  if err != nil {
+    fmt.Printf("%s\n", err)
+    return
+  }
+  
+  rand.Seed(time.now().Unix())
